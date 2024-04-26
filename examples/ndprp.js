c5e58009e4f73947019e8b3c6762e0f35fe18338
Andrew <andreabf2001@gmail.com>
2024-04-26 14:25:28 +0200

    Exercise on nodes hierarcy

---
+++ examples/ndprp.js
@@ -0,0 +1,7 @@
+/**
+ * There’s a tree structured as nested ul/li.
+ * Write the code that for each <li> shows:
+ * 1.What’s the text inside it (without the subtree)
+ * 2.The number of nested <li> – all descendants, including the deeply nested ones.
+ */
+
