34b858101e623ed1df2fa5b8b68b379390cdd098
Andrew <andreabf2001@gmail.com>
2024-05-02 11:14:46 +0200

    Chapter DOM modifications

--- examples/ndprp.js
+++ examples/ndprp.js
@@ -11,3 +11,18 @@ function a(){
     }
 
 }
+
+
+let elem = document.querySelector('[data-widget-name]'); // get element with attribute data-widget-name
+alert(elem.dataset.widgetName); //get attribute value 
+
+
+
+let links = document.querySelectorAll('a'); //gets all link elements (returns an iterable collection)
+for(let link of links){
+    let href = link.getAttribute('href');
+    if(!href)continue;
+    if(! href.includes('://')) continue;
+    if(href.startsWith('https://internal.com')) continue;
+    link.style.color = 'orange';
+}
